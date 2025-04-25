effff6250bd27fae91c237d024b3fd75fbed867c
Stephanie <stephaniecsilva88@gmail.com>
2025-04-25 19:57:10 -0300

    .

---
+++ src/main/java/br/ueg/progwebi/pacienteapi/PacienteapiApplication.java
@@ -0,0 +1,13 @@
+package br.ueg.progwebi.pacienteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacienteapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacienteapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ueg/progwebi/pacienteapi/PacienteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.progwebi.pacienteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacienteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
