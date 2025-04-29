e3e4b0bb293d0697ad7051ed670187780b0d124a
Stéphanie Christine <stephaniecsilva88@gmail.com>
2025-04-29 04:37:19 -0300

    tentando conectar a base de dados

--- src/main/java/br/ueg/progwebi/pacienteapi/model/Paciente.java
+++ src/main/java/br/ueg/progwebi/pacienteapi/model/Paciente.java
@@ -1,5 +1,8 @@
 package br.ueg.progwebi.pacienteapi.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -10,8 +13,13 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
 public class Paciente {
+    @Id
+    @Column(name = "PK")
     private long id;
+
+    @Column(lenght = 200)
     private String nome;
     private String cpf;
     private LocalDate dataNascimento;
